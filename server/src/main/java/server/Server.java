aabfde18f3c88468415cb7b7c209f13bb6b7dd1b
michaelharris1525@gmail.com <michaelharris1525@gmail.com>
2024-11-05 13:21:20 -0700

    Made an extra classs file for myasdfa server, made them into functions to basically shorten my 300 lines of code to less than a 100 for my server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,19 +3,10 @@ package server;
 import com.google.gson.Gson;
 import dataaccess.*;
 import model.*;
-import requestextension.JoinRequestIsNull;
 import spark.*;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import dataaccess.UserSQLDao;
 
 public class Server {
-    private final UserSQLDao userDataobj = new UserSQLDao();
-    private final Service serviceobj = new Service();
-    private final AuthSQLTokenClass authTokenData = new AuthSQLTokenClass();
-    private final GameSQLDao gameData = new GameSQLDao();
-
     //SQL Databases
     private final UserSQLDao userSQL = new UserSQLDao();
     private final GameSQLDao gameSQLDAO = new GameSQLDao();
@@ -24,15 +15,6 @@ public class Server {
     //Service Helper
     private final ServerHelper serverFunctions = new ServerHelper();
 
-    private String handleException(Exception ex, Response res, Gson serializer) {
-        if (ex instanceof com.google.gson.JsonSyntaxException) {
-            res.status(400); // Bad request for invalid JSON
-            return serializer.toJson(new ErrorData("Invalid JSON format."));
-        } else {
-            res.status(500); // Internal Server Error
-            return serializer.toJson(new ErrorData("Error: (description of error)"));
-        }
-    }
     public void setUpServer() {
         try {
             // Create tables if they don't exist
@@ -67,6 +49,7 @@ public class Server {
         Spark.put("/game", serverFunctions::userJoinGame);
         //Clearing it all
         Spark.delete("/db", serverFunctions::userClearDb);
+
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
         Spark.awaitInitialization();
